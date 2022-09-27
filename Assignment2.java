1f8acf7749fce9f639654ebcbbfaaee91d7d84f0
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-27 16:19:04 -0400

    Updated to correct code directory
    Last one was wrong

--- Assignment2.java
+++ Assignment2.java
@@ -1,18 +1,16 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Assignment2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; i < 10; j++) {
-                System.out.println ("i is: " + i + ", h is: " + j);
-        }
-    }
-    int i = 0;
-    int j = 0;
-
-    while (i < 10) {
-        while (j < 10) {
-        }
+        int Number;                                                     //Input variable
+        System.out.println ("Enter a number:");
+        Scanner scan = new Scanner(System.in);
+        Number = scan.nextInt();                                              //Stores input
 
 
-        }
+
+        for (int i = 0; i == Number; i++)
+
+        System.out.println ("You got number:" + Number);
     }
 }
