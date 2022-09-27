e72c121cfb5db322bff0a03fced35a018545caba
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-27 16:14:51 -0400

    Testing for loops
    Understanding how to manipulate for loop

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,18 @@
 public class Assignment2 {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; i < 10; j++) {
+                System.out.println ("i is: " + i + ", h is: " + j);
+        }
+    }
+    int i = 0;
+    int j = 0;
+
+    while (i < 10) {
+        while (j < 10) {
+        }
+
+
+        }
     }
 }
