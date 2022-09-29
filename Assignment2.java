a304ef999c67268e6bdef6468f8b5b7e5822ecf4
CamDas <111987807+CamDas@users.noreply.github.com>
2022-09-29 17:03:15 -0400

    Algorithm
    Trying to understand

--- Assignment2.java
+++ Assignment2.java
@@ -2,15 +2,32 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class Assignment2 {
     public static void main(String[] args) {
-        int Number;                                                     //Input variable
+        double number;
+        double space = 0;
+        double column;
+        double row;
         System.out.println ("Enter a number:");
         Scanner scan = new Scanner(System.in);
-        Number = scan.nextInt();                                              //Stores input
+        number = scan.nextDouble();
 
 
+        number = number / 2.1;                                          //Breaks input in half
 
-        for (int i = 0; i == Number; i++)
+        if (space != number) {
+            for (space = 0; space > number; space ++)
+            System.out.print(" ");
+            }
+        else
 
-        System.out.println ("You got number:" + Number);
+            for (space = 0; space > number; space ++)
+            System.out.print("*");
+
+        for (column = 0; column < number; column++) {
+            for (row = 0; row < column; row++) {
+                System.out.print("**");
+            }
+            System.out.print("*\n");
+        }
     }
 }
+
