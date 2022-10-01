7ece6205277517533b19f7c43004bafe1e41ecf7
CamDas <111987807+CamDas@users.noreply.github.com>
2022-10-01 14:09:29 -0400

    Top half of odd diamond
    Working code for top half of odd diamond

--- Assignment2.java
+++ Assignment2.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        int number;
-        int space;
-        int row;
+        double number;
+        double space;
+        double row;
         System.out.println ("Enter a number:");
         Scanner scan = new Scanner(System.in);
-        number = scan.nextInt();
+        number = scan.nextDouble();
 
-        for (space = 1; space < number; space++) {
+        number = number / 1.5;                                        //Breaks up input in half
+
+        for (space = 1; space <= number; space++) {                   //Top half of diamond
             for (row = space; row <= number; row++) {
-                System.out.print (" ");
+                System.out.print(" ");
+            }
+            for (row = 1; row < space; row++) {
+                System.out.print("**");
             }
             for (row = 1; row <= space; row++) {
-                System.out.print("*");
             }
-        }
+                System.out.print("*\n");
+            }
+
     }
 }
